e1f6c4645b76f58f03ebe2717f44bde1516a7a0a
Tajcore <45900878+Tajcore@users.noreply.github.com>
2020-04-11 14:01:30 -0500

    Database Update

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ UPLOAD_FOLDER = './app/static/Profile_Pictures'
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://aimdukhdmkhpxj:abaace9533cc39e4a14b25c2693927ed7fe5972fbc1a42987c3987f79baab7df@ec2-54-147-209-121.compute-1.amazonaws.com:5432/dfbhe6ai7ih0cg"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://pkmlezjsgguodb:98697071ad427262df495a7689b76393d4a6ca3fda3222b60512ae256e9151a8@ec2-34-235-108-68.compute-1.amazonaws.com:5432/d1672ehihq0i6m"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True 
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
