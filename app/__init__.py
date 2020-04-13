a0e244c5209c983d114a919130d58ea1e3bc4c66
Tajcore <45900878+Tajcore@users.noreply.github.com>
2020-04-13 15:11:27 -0500

    bug fix #2

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = SECRET_KEY
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://whdrjgrihrhabz:372a621310977c01b4e828756de70e321b360c055203bdea1d771fcf1b696cb9@ec2-23-20-129-146.compute-1.amazonaws.com:5432/dbcgs1qd7ovmup"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://yxscqrinjaicrk:63f01346475afadb98dd9635e1410e6becbf30c5d59460aed5c7652ff7510ada@ec2-52-7-39-178.compute-1.amazonaws.com:5432/d5ccae1rhr7fae"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
